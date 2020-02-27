75a2360d1004c2c8a8e1f07122b564eeaed142df
eigi0621 <eigi0088@gmail.com>
2020-02-27 10:49:14 +0100

    tilføjet kommentarer, ryddet op

---
+++ links/nav-links.js
@@ -0,0 +1,95 @@
+//kald transition animation ud
+startAniOut();
+
+//alle eventlistenere til navigationslinks
+document.querySelector("#link-forside0").addEventListener("click", () => {
+  startAniIn(); //kald transition animation ind
+  setTimeout(function () {
+    //vent 600ms, gå til linket
+    location.href = "index.html"
+  }, 600);
+});
+document.querySelector("#link-forside1").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "index.html"
+  }, 600);
+});
+document.querySelector("#link-produkter1").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=alle"
+  }, 600);
+});
+document.querySelector("#link-alle1").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=alle"
+  }, 600);
+});
+document.querySelector("#link-jakker1").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=jakker"
+  }, 600);
+});
+document.querySelector("#link-kjoler1").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=kjoler"
+  }, 600);
+});
+document.querySelector("#link-toppe1").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=toppe"
+  }, 600);
+});
+document.querySelector("#link-om_os1").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "om_os.html"
+  }, 600);
+});
+document.querySelector("#link-forside2").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "index.html"
+  }, 600);
+});
+document.querySelector("#link-produkter2").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=alle"
+  }, 600);
+});
+document.querySelector("#link-alle2").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=alle"
+  }, 600);
+});
+document.querySelector("#link-jakker2").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=jakker"
+  }, 600);
+});
+document.querySelector("#link-kjoler2").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=kjoler"
+  }, 600);
+});
+document.querySelector("#link-toppe2").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "produkter.html?kategori=toppe"
+  }, 600);
+});
+document.querySelector("#link-om_os2").addEventListener("click", () => {
+  startAniIn();
+  setTimeout(function () {
+    location.href = "om_os.html"
+  }, 600);
+});
